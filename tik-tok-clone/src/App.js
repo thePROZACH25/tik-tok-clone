f3862d333ac33e58af1fa7dde685b506b2cd72f2
Zach S.L <zach.s.ledford@gmail.com>
2020-08-09 19:25:10 -0400

    setting up firebase config

--- tik-tok-clone/src/App.js
+++ tik-tok-clone/src/App.js
@@ -8,7 +8,15 @@ function App() {
     <div className="app">
       
       <div className="app___videos">
-        <Video />
+        <Video 
+        url="https://v16m.tiktokcdn.com/6f0775043c50179ef2ca9722ecd5928c/5f330917/video/tos/useast2a/tos-useast2a-ve-0068c004/0831670ad54b44309d85ffff4612188d/?a=1233&br=2580&bt=1290&cr=0&cs=0&dr=0&ds=3&er=&l=202008092109420101902192230746630E&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=am9mdzc5dWlkdjMzNjczM0ApaTRlZWc4ODxnNztpaDY0Z2czX19wcGVrbjVfLS0xMTZzc2JiLTZhNS4uXy80YDZiMjM6Yw%3D%3D&vl=&vr="
+        channel="Zach"
+        description="I love it"
+        song="back that ass up"
+        likes={123}
+        messages={85}
+        shares={177}
+        />
         <Video />
         <Video />
         <Video />
--- tik-tok-clone/src/components/Video.js
+++ tik-tok-clone/src/components/Video.js
@@ -1,9 +1,9 @@
 import React, { useRef, useState } from "react";
 import VideoFooter from "./VideoFooter";
-import VideoSidebar from "./VideoSidebar"
+import VideoSidebar from "./VideoSidebar";
 import "./Video.css";
 
-function Video() {
+function Video({ url, channel, description, song, likes, messages, shares }) {
   const [play, setPlay] = useState(false);
   const videoRef = useRef(null);
 
@@ -24,14 +24,17 @@ function Video() {
         loop
         onClick={onVideoPress}
         ref={videoRef}
-        src="https://v16m.tiktokcdn.com/6f0775043c50179ef2ca9722ecd5928c/5f330917/video/tos/useast2a/tos-useast2a-ve-0068c004/0831670ad54b44309d85ffff4612188d/?a=1233&br=2580&bt=1290&cr=0&cs=0&dr=0&ds=3&er=&l=202008092109420101902192230746630E&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=am9mdzc5dWlkdjMzNjczM0ApaTRlZWc4ODxnNztpaDY0Z2czX19wcGVrbjVfLS0xMTZzc2JiLTZhNS4uXy80YDZiMjM6Yw%3D%3D&vl=&vr="
+        src={url}
       ></video>
       <VideoFooter
-        channel="thePROZACH"
-        description="YOOOO!!! What it do!!"
-        song="Slob on my nob - Three 6 Mafia"
+        channel={channel}
+        description={description}
+        song={song}
       />
-      <VideoSidebar likes={111} messages={222} shares={333}/>
+      <VideoSidebar 
+      likes={likes} 
+      messages={messages} 
+      shares={shares} />
     </div>
   );
 }
