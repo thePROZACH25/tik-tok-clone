1419b629a69a6af75243b13ed5b01ada07f0fab9
Zach S.L <zach.s.ledford@gmail.com>
2020-08-09 17:51:10 -0400

    adding props to video footer

--- tik-tok-clone/src/components/Video.js
+++ tik-tok-clone/src/components/Video.js
@@ -25,7 +25,11 @@ function Video() {
         ref={videoRef}
         src="https://v16m.tiktokcdn.com/6f0775043c50179ef2ca9722ecd5928c/5f330917/video/tos/useast2a/tos-useast2a-ve-0068c004/0831670ad54b44309d85ffff4612188d/?a=1233&br=2580&bt=1290&cr=0&cs=0&dr=0&ds=3&er=&l=202008092109420101902192230746630E&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=am9mdzc5dWlkdjMzNjczM0ApaTRlZWc4ODxnNztpaDY0Z2czX19wcGVrbjVfLS0xMTZzc2JiLTZhNS4uXy80YDZiMjM6Yw%3D%3D&vl=&vr="
       ></video>
-      <VideoFooter />
+      <VideoFooter
+        channel="thePROZACH"
+        description="YOOOO!!! What it do!!"
+        song="Slob on my nob - Three 6 Mafia"
+      />
     </div>
   );
 }
