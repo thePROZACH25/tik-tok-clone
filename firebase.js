3ad0f2bcb70ea9088b24991bc9ae2519485a8e19
Zach S.L <zach.s.ledford@gmail.com>
2020-08-09 13:53:43 -0400

    firebase config file

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZSXqPEVoSYqkoqPDoiDGZbfzZo2VAniQ",
+    authDomain: "tik-tok-clone-7d4b7.firebaseapp.com",
+    databaseURL: "https://tik-tok-clone-7d4b7.firebaseio.com",
+    projectId: "tik-tok-clone-7d4b7",
+    storageBucket: "tik-tok-clone-7d4b7.appspot.com",
+    messagingSenderId: "887003039785",
+    appId: "1:887003039785:web:059865044274369479b438",
+    measurementId: "G-Z8YJ81DV7V"
+  };
